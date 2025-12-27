3a749e9415956c87a4b699b28d6cc61a5065075f
feixiang <568980683@qq.com>
2025-12-27 11:48:06 +0800

    Initial Commit

---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by feixiang yang on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct EarthLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
